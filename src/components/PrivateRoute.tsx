77895492fd1ad5cc0f6d8144ab7c1a9a17cd144b
khainvit <khainvit@gmail.com>
2023-05-10 17:19:05 +0700

    push code trc khi ve

---
+++ src/components/PrivateRoute.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Route, RouteProps } from 'react-router-dom'
+import { Login } from '../pages/Acount/Login'
+
+export const PrivateRoute = ({children,...rest}:RouteProps):JSX.Element => {
+  return (
+    <Route {...rest} render={()=>{false ? children : <Login/>}}></Route>
+  )
+}
