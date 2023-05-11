56ac140983be0fbf34969918be189a20d550d78a
khainvit <khainvit@gmail.com>
2023-05-11 09:21:48 +0700

    sua file src/app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import './styles/sb-admin-2.min.css';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Switch,Switch } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import { Login } from './pages/Acount';
 import { PrivateRoute } from './components';
 
